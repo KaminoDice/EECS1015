2d73bd943d94baac78d924d5ccaacea3077523a7
Sakura Tsuki <tsukitosakura@icloud.com>
2022-11-11 14:07:59 -0500

    lab6 update

--- lab6.py
+++ lab6.py
@@ -2,21 +2,42 @@
 # EECS1015 - Fall 2022
 # Lab
 #####################################
+import random
+
 
 def print_student_info():
-    print("Name: XXXX")
-    print("ID: XXXXX")
+    print("Name: Cao Huanrui")
+    print("ID: 219156809")
     print("Section A")
-    print("email: xxxxxx")
+    print("email: saikoro@my.yorku.ca")
+
 
 def task0():
     print_student_info()
 
+
 # Functions for task 1
 # Write the function average_num() outside your task1() function.
+def average_num(*nums):
+    sum = 0
+    for i in nums:
+        sum = sum + i
+    avg = sum / len(nums)
+    return avg
+
 
 def task1():
-    pass
+    YN = 'Y'
+    while YN == 'Y':
+        num_amount = int(input("Input 4 or 5 numbers? "))
+        if num_amount == 4:
+            x1, x2, x3, x4 = [int(x) for x in input("Input 4 numbers [x1, x2, x3, x4]    : ").split(",")]
+            print("Average is %.2f" % average_num(x1, x2, x3, x4))
+        elif num_amount == 5:
+            x1, x2, x3, x4, x5 = [int(x) for x in input("Input 5 numbers [x1, x2, x3, x4, x5]: ").split(",")]
+            print("Average is %.2f" % average_num(x1, x2, x3, x4, x5))
+        YN = input("Try again? ").strip().upper()
+
 
 # Task 2
 # Write function build_stock_dict() here.
@@ -25,61 +46,143 @@ def task1():
 def print_stock_dict(stock_dict):
     keys = list(stock_dict.keys())
     print("{:10s} {:6s}  {}".format("Symbol", "Price", "Company Name"))
-    print("-"*31)
+    print("-" * 31)
     for k in keys:
-        print(f"{k:7s} {stock_dict[k][1]:8.2f}   {stock_dict[k][0]}") 
+        print(f"{k:7s} {stock_dict[k][1]:8.2f}   {stock_dict[k][0]}")
         # stock_dict[k][1]
         # ^^^^^^^^^^^^^    <- this gets the list for the key k
         #              ^^^ <- this retrieves item [1] in the list (price of stock)
         #
-    print() # <- an extra empty print to make it look nice
+    print()  # <- an extra empty print to make it look nice
+
+
+def string_to_stock_dict(astring):
+    stock_dict = {}
+    string_split = astring.split(" ")
+    for item in string_split:
+        stock_list = ["CN", 0.00]
+        stock_price = float(item.split(":")[1])
+        stock_list[0] = item.split(":")[0]
+        stock_list[1] = stock_price
+        stock_dict[item.split(":")[2]] = stock_list[:]
+    return stock_dict
 
 
 def task2():
     stock_dict1 = {"SNAP": ["Snap", 10.08], "PINS": ["Pinterest", 29.40], "GOOG": ["Google", 96.58]}
     stock_list_string = "Apple:155.74:AAPL Tesla:228.52:TSLA Ford:13.26:F Microsoft:9.12:MSFT Shopify:34.19:SHOP"
     print_stock_dict(stock_dict1)
-    #uncomment code for task4
-    #stock_dict2 = build_stock_dict(stock_list_string)
-    #print_stock_dict(stock_dict2)
+    # uncomment code for task4
+    # stock_dict2 = build_stock_dict(stock_list_string)
+    stock_dict2 = string_to_stock_dict(stock_list_string)
+    print_stock_dict(stock_dict2)
+
 
 # Task 3 functions create_rand_list(), delete_list_item(), print_list() here ourside task3()
+def create_rand_list():
+    rand_list = []
+    num_of_element = random.randint(5, 15)
+    min_value = random.randint(5, 10)
+    max_value = random.randint(20, 50)
+    for i in range(num_of_element):
+        rand_gen = random.randint(min_value, max_value)
+        while rand_gen in rand_list:
+            rand_gen = random.randint(min_value, max_value)
+        rand_list.append(rand_gen)
+    return rand_list
+
+
+def print_list(a_list):
+    print("--list--")
+    list_len = len(a_list)
+    if list_len == 0:
+        print("empty")
+    else:
+        for i in range(len(a_list)):
+            print("(%d)->" % a_list[i], end="")
+        print("(end)")
+
+
+def delete_list_item(a_list, item):
+    index = -1
+    if item in a_list:
+        index = a_list.index(item)
+    return index
+
+
 def task3():
-    pass
+    YN = 'Y'
+    rand_list = create_rand_list()
+    while YN == 'Y':
+        print_list(rand_list)
+        item_del = int(input("Item to delete: "))
+        del_index = delete_list_item(rand_list, item_del)
+        if del_index == -1:
+            print("Item %d could not be deleted. " % item_del)
+        else:
+            del rand_list[del_index]
+            print("Item %d successfully deleted at postion %d" % (item_del, del_index))
+        YN = input("Delete item [Y/N]? ").upper()
+
 
 # Task 4
 # Write functions print_image and uncompress_rle_image() here
 def print_image(image):
     pass
 
+
 def uncompress_rle_image(rle_image):
     pass
 
+
 def task4():
     rle_image1 = [[(5, '-')], [(2, ' '), (1, '|')], [(2, ' '), (1, '|')], [(1, ' '), (3, '-')]]
-    rle_image2 = [[(9, ' '), (1, '.'), (1, '8'), (1, '.'), (1, ' ')], [(9, ' '), (3, '8'), (1, ' ')], [(9, ' '), (3, '8'), (1, 'l')],
-     [(8, ' '), (1, 'j'), (4, '8'), (1, '.')], [(7, ' '), (1, '.'), (6, '8'), (1, '.')],
-     [(6, ' '), (1, '.'), (8, '8'), (1, '.')], [(4, ' '), (1, '.'), (1, 'd'), (10, '8'), (1, 'b'), (1, '.'), (1, ' ')],
-     [(2, ' '), (1, '.'), (1, 'd'), (14, '8'), (1, 'b'), (1, '.')], [(1, ' '), (1, '.'), (18, '8'), (1, 'b'), (1, '.')],
-     [(1, '.'), (21, '8')], [(22, '8')], [(3, '8'), (1, 'P'), (2, '"'), (1, '4'), (3, '8')],
-     [(1, '`'), (1, 'P'), (1, "'"), (5, ' '), (1, '.'), (4, ' '), (1, '.'), (5, ' '), (1, '`'), (1, 'q'), (1, "'")],
-     [(1, ' '), (1, '`'), (1, '-'), (2, '.'), (4, '_'), (1, ':'), (2, ' '), (1, ':'), (4, '_'), (2, '.'), (1, '-'),
-      (1, "'"), (1, ' ')], [(9, ' '), (1, ':'), (2, ' '), (1, ':')], [(9, ' '), (1, ':'), (2, ' '), (1, ':')],
-     [(9, ' '), (1, ':'), (2, ' '), (1, ':')], [(9, ' '), (1, ':'), (2, ' '), (1, ':')],
-     [(9, ' '), (1, ':'), (2, ' '), (1, ':')],
-     [(7, ' '), (1, '\\'), (1, '('), (1, '/'), (1, '\\'), (1, ')'), (1, '\\'), (1, '/'), (1, ' '), (1, 'm'), (1, 'h')]]
-    rle_image3 = [[(52, '.')], [(52, '.')], [(25, '.'), (1, '/'), (1, '\\'), (25, '.')], [(18, '.'), (6, '_'), (1, '/'), (2, '_'), (1, '\\'), (7, '_'), (17, '.')], [(18, '.'), (2, '|'), (13, '-'), (2, '|'), (17, '.')], [(18, '.'), (2, '|'), (13, ' '), (2, '|'), (17, '.')], [(18, '.'), (2, '|'), (4, ' '), (1, '\\'), (3, '|'), (1, '/'), (4, ' '), (2, '|'), (17, '.')], [(18, '.'), (2, '|'), (3, ' '), (1, '['), (1, ' '), (1, '@'), (1, '-'), (1, '@'), (1, ' '), (1, ']'), (3, ' '), (2, '|'), (17, '.')], [(18, '.'), (2, '|'), (4, ' '), (1, '('), (1, ' '), (1, '.'), (1, ' '), (1, ')'), (4, ' '), (2, '|'), (7, '.'), (7, ' '), (3, '.')], [(18, '.'), (2, '|'), (4, ' '), (1, '_'), (1, '('), (1, 'O'), (1, ')'), (1, '_'), (4, ' '), (2, '|'), (7, '.'), (1, '|'), (1, 'E'), (1, 'X'), (1, 'I'), (1, 'T'), (1, ' '), (1, '|'), (3, '.')], [(18, '.'), (2, '|'), (3, ' '), (1, '/'), (1, ' '), (1, '>'), (1, '='), (1, '<'), (1, ' '), (1, '\\'), (3, ' '), (2, '|'), (7, '.'), (1, '|'), (2, '='), (2, '>'), (1, ' '), (1, '|'), (3, '.')], [(18, '.'), (2, '|'), (2, '_'), (1, '/'), (1, '_'), (1, '|'), (1, '_'), (1, ':'), (1, '_'), (1, '|'), (1, '_'), (1, '\\'), (2, '_'), (2, '|'), (17, '.')], [(18, '.'), (17, '-'), (17, '.')], [(52, '.')], [(52, '.')]]
-    
+    rle_image2 = [[(9, ' '), (1, '.'), (1, '8'), (1, '.'), (1, ' ')], [(9, ' '), (3, '8'), (1, ' ')],
+                  [(9, ' '), (3, '8'), (1, 'l')],
+                  [(8, ' '), (1, 'j'), (4, '8'), (1, '.')], [(7, ' '), (1, '.'), (6, '8'), (1, '.')],
+                  [(6, ' '), (1, '.'), (8, '8'), (1, '.')],
+                  [(4, ' '), (1, '.'), (1, 'd'), (10, '8'), (1, 'b'), (1, '.'), (1, ' ')],
+                  [(2, ' '), (1, '.'), (1, 'd'), (14, '8'), (1, 'b'), (1, '.')],
+                  [(1, ' '), (1, '.'), (18, '8'), (1, 'b'), (1, '.')],
+                  [(1, '.'), (21, '8')], [(22, '8')], [(3, '8'), (1, 'P'), (2, '"'), (1, '4'), (3, '8')],
+                  [(1, '`'), (1, 'P'), (1, "'"), (5, ' '), (1, '.'), (4, ' '), (1, '.'), (5, ' '), (1, '`'), (1, 'q'),
+                   (1, "'")],
+                  [(1, ' '), (1, '`'), (1, '-'), (2, '.'), (4, '_'), (1, ':'), (2, ' '), (1, ':'), (4, '_'), (2, '.'),
+                   (1, '-'),
+                   (1, "'"), (1, ' ')], [(9, ' '), (1, ':'), (2, ' '), (1, ':')],
+                  [(9, ' '), (1, ':'), (2, ' '), (1, ':')],
+                  [(9, ' '), (1, ':'), (2, ' '), (1, ':')], [(9, ' '), (1, ':'), (2, ' '), (1, ':')],
+                  [(9, ' '), (1, ':'), (2, ' '), (1, ':')],
+                  [(7, ' '), (1, '\\'), (1, '('), (1, '/'), (1, '\\'), (1, ')'), (1, '\\'), (1, '/'), (1, ' '),
+                   (1, 'm'), (1, 'h')]]
+    rle_image3 = [[(52, '.')], [(52, '.')], [(25, '.'), (1, '/'), (1, '\\'), (25, '.')],
+                  [(18, '.'), (6, '_'), (1, '/'), (2, '_'), (1, '\\'), (7, '_'), (17, '.')],
+                  [(18, '.'), (2, '|'), (13, '-'), (2, '|'), (17, '.')],
+                  [(18, '.'), (2, '|'), (13, ' '), (2, '|'), (17, '.')],
+                  [(18, '.'), (2, '|'), (4, ' '), (1, '\\'), (3, '|'), (1, '/'), (4, ' '), (2, '|'), (17, '.')],
+                  [(18, '.'), (2, '|'), (3, ' '), (1, '['), (1, ' '), (1, '@'), (1, '-'), (1, '@'), (1, ' '), (1, ']'),
+                   (3, ' '), (2, '|'), (17, '.')],
+                  [(18, '.'), (2, '|'), (4, ' '), (1, '('), (1, ' '), (1, '.'), (1, ' '), (1, ')'), (4, ' '), (2, '|'),
+                   (7, '.'), (7, ' '), (3, '.')],
+                  [(18, '.'), (2, '|'), (4, ' '), (1, '_'), (1, '('), (1, 'O'), (1, ')'), (1, '_'), (4, ' '), (2, '|'),
+                   (7, '.'), (1, '|'), (1, 'E'), (1, 'X'), (1, 'I'), (1, 'T'), (1, ' '), (1, '|'), (3, '.')],
+                  [(18, '.'), (2, '|'), (3, ' '), (1, '/'), (1, ' '), (1, '>'), (1, '='), (1, '<'), (1, ' '), (1, '\\'),
+                   (3, ' '), (2, '|'), (7, '.'), (1, '|'), (2, '='), (2, '>'), (1, ' '), (1, '|'), (3, '.')],
+                  [(18, '.'), (2, '|'), (2, '_'), (1, '/'), (1, '_'), (1, '|'), (1, '_'), (1, ':'), (1, '_'), (1, '|'),
+                   (1, '_'), (1, '\\'), (2, '_'), (2, '|'), (17, '.')], [(18, '.'), (17, '-'), (17, '.')], [(52, '.')],
+                  [(52, '.')]]
+
     # uncomment code for task4
-    #print("\t\tImage 1\n")
-    #image1 = uncompress_rle_image(rle_image1)
-    #print_image(image1)
-    #print("\t\tImage 2\n")
-    #image2 = uncompress_rle_image(rle_image2)
-    #print_image(image2)
-    #print("\t\tImage 3\n")
-    #image3 = uncompress_rle_image(rle_image3)
-    #print_image(image3)
+    # print("\t\tImage 1\n")
+    # image1 = uncompress_rle_image(rle_image1)
+    # print_image(image1)
+    # print("\t\tImage 2\n")
+    # image2 = uncompress_rle_image(rle_image2)
+    # print_image(image2)
+    # print("\t\tImage 3\n")
+    # image3 = uncompress_rle_image(rle_image3)
+    # print_image(image3)
+
 
 def main():
     task0()
@@ -93,6 +196,7 @@ def main():
     task4()
 
     input("Press enter to end lab 6.")
+
 
 if __name__ == '__main__':
     main()
